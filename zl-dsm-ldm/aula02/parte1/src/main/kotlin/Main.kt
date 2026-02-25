4059c8d198df9b16accf08361c2906d120a2f71e
Antonio Carvalho <antoniorcn@hotmail.com>
2026-02-25 11:03:27 -0300

    zl-dsm-ldm aula 02 parte 2

--- zl-dsm-ldm/aula02/parte1/src/main/kotlin/Main.kt
+++ zl-dsm-ldm/aula02/parte1/src/main/kotlin/Main.kt
@@ -10,9 +10,42 @@ package edu.curso
 //    }
 //}
 
-data class Aluno ( val nome : String = "" )
+data class Aluno ( val nome : String = "" ) {
+    operator fun plus( outro : Aluno ) : List<Aluno> {
+        return listOf(this, outro)
+    }
+
+    operator fun times( numero : Int ) : List<Aluno> {
+        val lista = mutableListOf<Aluno>()
+        for ( i in 1 .. numero ) {
+            lista.add(this)
+        }
+        return lista
+    }
+}
+
+fun String.centralizar( largura : Int ) : String {
+    val espacos = (largura - this.length) / 2
+    return "${" ".repeat(espacos)}$this${" ".repeat(espacos)}"
+}
+
+/**
+ *  Cria um cabeçalho usando o caracterre (=)
+ */
+fun imprimirCabecalho(nomeApp : String, autor : String = "" ) {
+    // val espacosApp = (50 - nomeApp.length) / 2
+    // val espacosAutor = (50 - autor.length) / 2
+    println("=".repeat(50))
+    // println("=${" ".repeat(espacosApp)}$nomeApp=${" ".repeat(espacosApp)}")
+    // println("=${" ".repeat(espacosAutor)}$autor=${" ".repeat(espacosAutor)}")
+    println("=${nomeApp.centralizar(48)}=")
+    println("=${autor.centralizar(48)}=")
+    println("=".repeat(50))
+}
 
 fun main() {
+    imprimirCabecalho("Sistema de Teste do Kotlin")
+    imprimirCabecalho(autor = "Joao Silva", nomeApp = "Sistema de Teste do Kotlin")
     var nome : String? = null
     nome = "Joao"
 
@@ -20,14 +53,32 @@ fun main() {
     val a2 = Aluno( "Joao Silva")
     println("Hello World")
 
-    nome = "Maria Silva"
+    val a3 = a1 + a2
+    println("A soma dos alunos deu $a3")
+
+    val a4 = a1 * 8
+    println("A1 * 8 deu $a4")
+
+    var o : Any = "Texto String"
+    o = 54
+    o = true
+
+    // nome = "Maria Silva"
 
     println(a1)
 
+    if ( o is String ) {
+        o.uppercase()
+    }
+
+    println("Nome em maiusculo: ${4 + 5} ${nome!!.uppercase()}")
+
     if (nome != null) {
-        println("Nome em maiusculo: ${nome.uppercase()}")
+        println("Nome em maiusculo: ${4 + 5} ${nome.uppercase()}")
     }
     println("Nome em maiusculo: ${nome?.uppercase()}")
+
+    println("Nome em maiusculo: ${nome?.uppercase() ?: "Nome não definido"}")
 
     // System.out.println("Teste")
 
@@ -45,8 +96,8 @@ fun main() {
 
     val valorInteiro = "101".toInt()
 
-    println("Digite seu nome: ")
-    val nomCompleto = readln() // ou readLine()
-    println("Olá $nomCompleto")
+//    println("Digite seu nome: ")
+//    val nomCompleto = readln() // ou readLine()
+//    println("Olá $nomCompleto")
 
 }
