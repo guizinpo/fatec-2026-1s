5521112bc221b33f1ba5c113d45c4703846c8fd0
Antonio Carvalho <antoniorcn@hotmail.com>
2026-02-26 16:06:15 -0300

    zl-ads-poo-tarde aula 03 parte 1

---
+++ zl-ads-poo-tarde/aula03/edu/curso/Teste.java
@@ -0,0 +1,20 @@
+package edu.curso;
+
+public class Teste { 
+
+    public static void main(String[] args) {
+
+        System.out.println("Incio do codigo");
+
+        // inicioCodigo:      
+        while(true) {   
+            System.out.println("Executando Linha1");
+            System.out.println("Executando Linha2");
+            System.out.println("Executando Linha3");
+            // if( 1==1) {
+            //     continue inicioCodigo;
+            // }
+            // System.out.println("Esta linha nunca será executada");
+       }
+    }
+}
